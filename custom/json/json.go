a9b1f7b86c842ad919e2830447d42d6cdd9ea32c
Andrey Dmitriev <dmitriev@corp.mail.ru>
2019-02-24 01:23:14 +0300

    fix commit

---
+++ custom/json/json.go
@@ -0,0 +1,55 @@
+package customjson
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+func CustomMarshal(v interface{}) ([]byte, error) {
+	what := reflect.TypeOf(v)
+	val := reflect.ValueOf(v)
+	switch what.Kind() {
+	case reflect.Array:
+		fallthrough
+	case reflect.Slice:
+		buffer := bytes.NewBufferString("[")
+		sarr := val.Len()
+		for i := 0; i < sarr; i++ {
+			tmp, err := CustomMarshal(val.Index(i).Interface())
+			if err != nil {
+				return nil, err
+			}
+			buffer.WriteString(fmt.Sprintf("%s", tmp))
+			if sarr > 1 && i < sarr-1 {
+				buffer.WriteString(",")
+			}
+		}
+		buffer.WriteString("]")
+		return buffer.Bytes(), nil
+	case reflect.Struct:
+		fallthrough
+	case reflect.Chan:
+		return nil, errors.New("dont support type")
+	case reflect.Map:
+		buffer := bytes.NewBufferString("{")
+		for _, k := range val.MapKeys() {
+			tmp_key, err := CustomMarshal(k.Interface())
+			if err != nil {
+				return nil, err
+			}
+			tmp_val, err := CustomMarshal(val.MapIndex(k).Interface())
+			if err != nil {
+				return nil, err
+			}
+			buffer.WriteString(fmt.Sprintf("%s: %s,", tmp_key, tmp_val))
+		}
+		buffer.WriteString("}")
+		return buffer.Bytes(), nil
+	default:
+		return json.Marshal(v)
+	}
+	return json.Marshal(v)
+}
