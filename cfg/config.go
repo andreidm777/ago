125db39c4622c7dfd8d52fb5d4eef3c24c5dab31
Andrey Dmitriev <dmitriev@corp.mail.ru>
2020-01-29 18:42:52 +0300

    fix config

--- cfg/config.go
+++ cfg/config.go
@@ -1,6 +1,8 @@
 package cfg
 
 import (
+    //"fmt"
+    log "github.com/sirupsen/logrus"
     "io/ioutil"
 	"os"
     "reflect"
@@ -27,10 +29,42 @@ func create() *Config {
 
 var config = create()
 
+func typeBuild(v interface{}, val ParamValue) {
+    switch val.t {
+        case reflect.Int:
+            i := val.v.(*int)
+            *i = v.(int)
+        case reflect.String:
+            i := val.v.(*string)
+            *i = v.(string)
+        case reflect.Bool:
+            i := val.v.(*bool)
+            *i = v.(bool)
+        case reflect.Slice:
+            if i, ok := val.v.(*[]string) ; ok {
+                if len(*i) > 0 {
+                    *i = nil
+                }
+                for _, vi := range v.([]interface{}) {
+                    *i = append(*i, vi.(string))
+                }
+            } else if i, ok := val.v.(*[]int) ; ok {
+                if len(*i) > 0 {
+                    *i = nil
+                }
+                for _, vi := range v.([]interface{}) {
+                    *i = append(*i, vi.(int))
+                }
+            }
+        default:
+    }
+}
+
 func Parse(filename string) error {
     config.Fname = filename
 	file, err   := os.Open(config.Fname)
 	if err != nil {
+        log.Warn(err)
 		return err
 	}
 	
@@ -38,6 +72,7 @@ func Parse(filename string) error {
 	
 	buffer, err := ioutil.ReadAll(file)
     if err != nil {
+        log.Warn(err)
         return err
     }
 
@@ -46,19 +81,22 @@ func Parse(filename string) error {
     err = yaml.Unmarshal(buffer, &values)
     if err == nil {
         for k, v := range values {
-            if val, ok := config.bindings[k]; ok {
+             if val, ok := config.bindings[k]; ok {
                 if reflect.TypeOf(v).Kind() == val.t {
-                    switch val.t {
-                        case reflect.Int:
-                            i := val.v.(*int)
-                            *i = v.(int)                
-                        default:
-                    }
+                    typeBuild(v, val)
                 }
             }
         }
     }
+    log.Warn("parse success")
     return err
+}
+
+func Reload() {
+    err := Parse(config.Fname)
+    if err != nil {
+        log.Warn(err)
+    }
 }
 
 func AddBinding(name string, desc string, val interface{}) interface{} {
@@ -66,7 +104,7 @@ func AddBinding(name string, desc string, val interface{}) interface{} {
         if tmp.t == reflect.ValueOf(val).Elem().Kind() {
             return tmp.v
         } else {
-            panic("bad type")
+            log.Fatalf("bad type %v %v", tmp.t, reflect.ValueOf(val).Elem().Kind())
         }
     } else {
         config.bindings[name]     = ParamValue{ v:val, t: reflect.ValueOf(val).Elem().Kind() }
@@ -80,6 +118,44 @@ func Int(name string, defaultval int, desc string ) *int {
     ret := AddBinding(name, desc, v)
     
     v = ret.(*int)
+    *v = defaultval
+    return v
+}
+
+func StringSlice(name string, defaultval []string, desc string) *[]string {
+    v   := new([]string)
+    ret := AddBinding(name, desc, v)
+    v = ret.(*[]string)
+    for _, val := range defaultval {
+		*v = append(*v, val)
+	}
+    return v
+}
+
+func IntSlice(name string, defaultval []int, desc string) *[]int {
+    v   := new([]int)
+    ret := AddBinding(name, desc, v)
+    v = ret.(*[]int)
+    for _, val := range defaultval {
+		*v = append(*v, val)
+	}
+    return v
+}
+
+func String(name string, defaultval string, desc string ) *string {
+    v := new(string)
+    ret := AddBinding(name, desc, v)
+    
+    v = ret.(*string)
+    *v = defaultval
+    return v
+}
+
+func Bool(name string, defaultval bool, desc string ) *bool {
+    v := new(bool)
+    ret := AddBinding(name, desc, v)
+    
+    v = ret.(*bool)
     *v = defaultval
     return v
 }
--- cfg/config_test.go
+++ cfg/config_test.go
@@ -2,12 +2,26 @@ package cfg
 
 import (
     "fmt"
+    //"reflect"
     "testing"
+    log "github.com/sirupsen/logrus"
+    "log/syslog"
 )
 
 var i = Int("test", 1, "test")
 
+var a = IntSlice("test_1", []int{1,2}, "test 1")
+var c = Bool("test_2", false, "test 1")
+var d = String("test_3", "test", "test 1")
+
 func TestConf(t *testing.T) {
+    logwriter, e := syslog.New(syslog.LOG_NOTICE, "myprog")
+    if e == nil {
+        log.SetOutput(logwriter)
+    }
     Parse("test.yaml")
     fmt.Printf("%d\n", *i)
+    fmt.Println(*a)
+    fmt.Println(*c)
+    fmt.Println(*d)
 }
